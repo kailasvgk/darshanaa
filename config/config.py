32cf8c0f3933d42623eff880def701a2665d422c
kailasvgk <118908365+kailasvgk@users.noreply.github.com>
2022-11-29 18:29:03 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -26,7 +26,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_kailas")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Darshana_music_support")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Pranav_support_Chat")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
